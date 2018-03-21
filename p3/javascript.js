9cda6c5992d873110b74d1201012954bfc86607e
Orlando J Betancourth <orlando.betancourth@gmail.com>
2018-03-20 20:54:46 -0600

    Javascript Syntaxis

---
+++ p3/javascript.js
@@ -0,0 +1,52 @@
+/*
+Comentario de multiple linea
+*/
+
+//Comentaio de una sola linea
+// sin nomenclatura NO USAR -300
+var cuandolosgatosnosestanlosratonesfiestashacen=true;
+
+// nomenclatura camel Case
+var cuandoLosGatosNoEstanLosRatonesFiestaHacen = true;
+// nomeclatura snake
+var cuando_los_gatos_no_estan_los_ratones_fiesta_hacen=true;
+
+// nomenclatura Hungara
+var intEdad = 24;
+var txtEdad ="24 Años";
+// Mantenible facil de entender y de modificar
+var intEdadPrimeraComunion = 8;
+
+// tipos de datos
+// boleanos true, false
+// numericos
+// texto
+// function
+
+// operadores aritmeticos
+var a = 10, b=11, c=0;
+// +
+// -
+c = a + b; // 21
+c +=  a ; //31
+c ++ ; // 32
+
+
+c = a - b; //-1
+c -= -10; // a -11  b 9
+c --; // 8
+
+c = a  * b ; //110;
+c *= a; //1100
+
+c = a / b;
+c /= b;
+
+// operadores lógicos
+a = 2;
+b = 2;
+if(a === b){
+  alert(true);
+} else{
+  alert(false);
+}
