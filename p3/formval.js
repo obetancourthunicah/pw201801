c196ae52f31b47a8a2197606534118e4e51b9d65
Orlando J Betancourth <orlando.betancourth@gmail.com>
2018-03-22 21:03:52 -0600

    Validación de Formularios con Javascript

---
+++ p3/formval.js
@@ -0,0 +1,35 @@
+//Declaraciones
+function btnGuardar_onClick(e){
+  e.preventDefault();
+  e.stopPropagation();
+  var error = false;
+  var txtNombre = document.getElementById('txtNombre');
+  if(txtNombre.value){
+    //https://regex101.com/
+    if((/^\s*$/).test(txtNombre.value)||!(/^[A-Za-z\sñÑáéíóúü']*$/).test(txtNombre.value)){
+      alert("El nombre tiene caracteres no válidos.")
+      txtNombre.focus();
+      error= true;
+    }
+  }else{
+    alert('El Nombre es Requerido');
+    txtNombre.focus();
+    error=true;
+  }
+
+  /// /^\(\+\d\d\d\)\s\d\d\d\d\-?\d\d\d\d$/
+
+  //Si No hay errores entonces enviar
+  if(!error){
+    var formulario = document.getElementById('');
+    formulario.submit();
+  }
+}
+
+// Vamos obtener la referecia al boton guardar
+
+// DOM Documento Object Model
+var btnGuardar = document.getElementById('btnGuardar');
+
+//Binding del evento click al boton btnGuardar
+btnGuardar.addEventListener('click',btnGuardar_onClick);
